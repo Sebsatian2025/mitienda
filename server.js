28bb9cf9a399ced9f04939d460d3401a981b2be5
kitdelanzamientodigital <sebastian_6435@hotmail.com>
2025-08-19 20:46:37 +0200

    Update server.js

--- server.js
+++ server.js
@@ -28,6 +28,12 @@ app.post('/crear-pago', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Servidor corriendo en puerto 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+    res.send("🚀 API activa y lista para recibir pagos");
+  });
+  
